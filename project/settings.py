00bbd6e89d3a2ccce9306ced8ceb98168bb4b9d9
Joshua Taylor Eppinette <josheppinette@icloud.com>
2017-02-02 16:54:22 -0500

    Update admin templates

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ LOGIN_URL = 'login'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
